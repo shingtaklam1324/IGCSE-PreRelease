f5b9cf37a018fc433ad81b941054f5b7a3a24223
Shing Tak Lam <shingtaklam1324@gmail.com>
2017-12-14 21:26:57 +0800

    update

--- 2018_MJ/prerelease.py
+++ 2018_MJ/prerelease.py
@@ -1,29 +1,50 @@
 # 2018 May/June CIE Comp Sci IGCSE Pre Release
 
 
-def convert_input(prompt="", to=float, err_msg="Error parsing input"):
+def convert_input(prompt="", to=float, err_msg="Error parsing input", validation=None):
     while 1:
         try:
-            return to(input(prompt))
+            value = to(input(prompt))
+            if validation is not None:
+                if validation(value):
+                    return value
+                else:
+                    print(err_msg)
+                    continue
+            else:
+                return value
         except ValueError:
             print(err_msg)
             continue
 
+def is_gt_zero(num):
+    return num > 0
 
 if __name__ == "__main__":
     cows = {}
 
-    num_cows = convert_input("Number of cows: ", to=int)
+    num_cows = convert_input(
+        prompt="Number of cows: ",
+        to=int,
+        validation=is_gt_zero,
+        err_msg="Invalid number of cows"
+    )
 
     for i in range(14):
-        print("Day {} {}".format(int(i / 2) + 1, "Morning" if i %
+        print("\n\nDay {} {}\n".format(int(i / 2) + 1, "Morning" if i %
                                  2 == 0 else "Afternoon"))
+        
         for c in range(num_cows):
             # If cow is in dict, use current, otherwise insert new list
             cow_id = input("Cow ID: ")
             current = cows.setdefault(cow_id, [])
 
-            volume = convert_input("Volume: ", to=float)
+            volume = convert_input(
+                prompt="Volume: ",
+                to=float,
+                validation=is_gt_zero,
+                err_msg="Invalid Volume input"
+            )
 
             current.append(volume)
             cows[cow_id] = current
@@ -32,7 +53,8 @@ if __name__ == "__main__":
     for cow in cows.values():
         total_vol += sum(cow)
 
-    print("\n\nTotal Volume: {} l".format(round(total_vol)))
+    print("\n\n===== Results =====")
+    print("\nTotal Volume: {} l".format(round(total_vol)))
     # Average = total / num of cows
     print("Average Volume: {} l".format(round(total_vol / len(cows.keys()))))
 
